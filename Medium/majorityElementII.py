73e5e75c8f48c25e3cc498fa1057664dcef8b814
yashpratap <pratapyashkulshrestha@gmail.com>
2025-03-17 22:27:06 +0530

    feat: finding majority element in an array (Medium)

---
+++ Medium/majorityElementII.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: list[int]) -> List[int]:
+        count = Counter(nums)
+        res = []
+
+        for key in count:
+            if count[key] > len(nums) // 3:
+                res.append(key)
+
+        return res
