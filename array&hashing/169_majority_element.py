5623782ad261c41cc701dc876a588c964aa5525b
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-29 22:37:06 +0530

    Problems: solved another problem - sort colors

--- array&hashing/169_majority_element.py
+++ array&hashing/169_majority_element.py
@@ -1,5 +1,5 @@
 class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
+    def majorityElementHM(self, nums: list[int]) -> int:
         hs = {}
         n = len(nums)
         for i in range(n):
@@ -11,6 +11,21 @@ class Solution:
             if hs[nums[i]] > n // 2:
                 return nums[i]
         return 0
+
+    def majorityElement(self, nums: list[int]) -> int:
+        c = 0
+        m = 0
+        for num in nums:
+            if c == 0:
+                m = num
+                c = 1
+            elif m == num:
+                c += 1
+
+            else:
+                c -= 1
+        return m
+
 
 solution = Solution()
 print(solution.majorityElement([2,2,1,1,1,2,2]))
