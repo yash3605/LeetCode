7379ddf0503db753e435ebcff34194c513612074
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-30 18:52:06 +0530

    Problem - Solved the Top K Frequent Element Problem

---
+++ array&hashing/encode_decode_string.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        return ""
+
+    def decode(self, s: str) -> list[str]:
+        return []
+
+solution = Solution()
+input = ["we", "say", ":", "yes"]
+encode = solution.encode(input)
