861733eda749ce9002c82cc7ec71f9d90b609201
yashprt <147393761+yash3605@users.noreply.github.com>
2026-07-02 02:39:39 +0530

    BinarySearch: Solved an easy question

---
+++ BinarySearch/69_sqrt_x.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        res = 0
+        while l <= r:
+            m = l + (r - l) //  2
+            if m * m > x:
+                r = m - 1
+            elif m * m < x:
+                l = m + 1
+                res = m
+            else:
+                return m
+        return res
+
+solution = Solution()
+print(solution.mySqrt(4))
+print(solution.mySqrt(8))
+print(solution.mySqrt(9))
+print(solution.mySqrt(13))
