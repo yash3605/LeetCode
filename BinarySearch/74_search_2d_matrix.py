ff0e71b09aab02d8254467adda044cee8eb70cfe
yashprt <147393761+yash3605@users.noreply.github.com>
2026-07-11 11:51:05 +0530

    BinarySearch: Solved a Medium Question

---
+++ BinarySearch/74_search_2d_matrix.py
@@ -0,0 +1,35 @@
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+
+        while top <= bot:
+            row = (top + bot)//2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            mid = (r + l)//2
+            if target > matrix[row][mid]:
+                l = mid + 1
+            elif target < matrix[row][mid]:
+                r = mid - 1
+            else:
+                return True
+        return False
+            
+
+
+solution = Solution()
+print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
+print(solution.searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 10))
+print(solution.searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 15))
